44d1c3b312b023aaf9af41213f64dde057538816
Anselmo <anselmo@dcm.com.ar>
2024-05-02 15:30:06 -0300

    test reference

--- src/server/api/routers/mobbex.ts
+++ src/server/api/routers/mobbex.ts
@@ -22,7 +22,7 @@ export const mobbexRouter = createTRPCRouter({
         apiKey: "zJ8LFTBX6Ba8D611e9io13fDZAwj0QmKO1Hn1yIj",
         accessToken: "d31f0721-2f85-44e7-bcc6-15e19d1a53cc",
       });
-
+      console.log(input.reference);
       const checkout = {
         total: input.amount,
         currency: "ARS",
