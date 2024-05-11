728ee9d08d7367d066820563aa76dd890e953947
Anselmo <anselmo@dcm.com.ar>
2024-05-11 02:24:41 -0300

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { env } from "process";
 import { db } from "~/server/db";
 
 export default async function middleware(request: NextRequest) {
@@ -7,23 +8,15 @@ export default async function middleware(request: NextRequest) {
   const { cookies } = request;
 
   let cookie = cookies.get("next-auth.session-token");
+
+  if (env.VERCE_ENV == "PRODUCITION") {
+    let cookie = cookies.get("__Secure-next-auth.session-token");
+  }
   const session = sessions.find(
     (session: any) => session.sessionToken === cookie?.value,
   );
   const access = result.find((role) => role.id === session?.user.role)?.access;
 
-  console.log("path is");
-  console.log(request.nextUrl.pathname);
-  console.log("cookies is");
-  console.log(cookies);
-  console.log("cokkie is");
-  console.log(cookie);
-  console.log("session is");
-  console.log(session);
-  console.log("result is");
-  console.log(result);
-  console.log("and roles allow to");
-  console.log(access);
   if (access === undefined) {
     return NextResponse.redirect(new URL("/accessdenied", request.url));
   }
