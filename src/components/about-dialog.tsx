aa1548afd246ae22dea660811eff06c074c8ef3d
Anselmo <anselmo@dcm.com.ar>
2024-05-11 02:25:25 -0300

    update middleware

--- src/components/about-dialog.tsx
+++ src/components/about-dialog.tsx
@@ -33,7 +33,7 @@ export function About() {
                 </div>
                 <div className="col-6">
                   <AlertDialogTitle className="pb-4">
-                    Versión 4.0
+                    Versión 4.1
                   </AlertDialogTitle>
                   <AlertDialogTitle>Fecha: 11 de mayo de 2024</AlertDialogTitle>
                 </div>
