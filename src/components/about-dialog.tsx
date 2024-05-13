0c5bbf383a42ee3c22a734c12f30437cdeeb8949
Anselmo <anselmo@dcm.com.ar>
2024-05-13 10:49:42 -0300

    fix user form v4.3.1

--- src/components/about-dialog.tsx
+++ src/components/about-dialog.tsx
@@ -33,9 +33,9 @@ export function About() {
                 </div>
                 <div className="col-6">
                   <AlertDialogTitle className="pb-4">
-                    Versión 4.3
+                    Versión 4.3.1
                   </AlertDialogTitle>
-                  <AlertDialogTitle>Fecha: 12 de mayo de 2024</AlertDialogTitle>
+                  <AlertDialogTitle>Fecha: 13 de mayo de 2024</AlertDialogTitle>
                 </div>
               </div>
               <AlertDialogDescription>
