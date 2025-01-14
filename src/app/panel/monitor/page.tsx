3a750cf168ba9a02c6beb716771a418ad5e9edd9
Anselmo <anseldalbene@gmail.com>
2025-01-13 23:04:26 -0300

    tesdt

--- src/app/panel/monitor/page.tsx
+++ src/app/panel/monitor/page.tsx
@@ -12,30 +12,31 @@ import { Locker } from "~/server/api/routers/lockers";
 export default async function Home() {
   const { lockers, stores, reservas } = await fetchData();
 
-  // return (
-  //   <section className="w-full">
-  //     <Carousel className="w-full">
-  //       <CarouselContent>
-  //         {lockers.map((locker) => {
-  //           const store = stores.find(
-  //             (store) => store.serieLocker === locker.nroSerieLocker,
-  //           );
+  return (
+    <section className="w-full">
+      <Carousel className="w-full">
+        <CarouselContent>
+          {lockers.map((locker) => {
+            const store = stores.find(
+              (store) => store.serieLocker === locker.nroSerieLocker,
+            );
 
-  //           return (
-  //             <CarouselItem key={locker.id}>
-  //               <Card>
-  //                 <CardTitle>
-  //                   <Header locker={locker} store={store} />
-  //                 </CardTitle>
-  //                 <BoxContent locker={locker} reservas={reservas} />
-  //               </Card>
-  //             </CarouselItem>
-  //           );
-  //         })}
-  //       </CarouselContent>
-  //     </Carousel>
-  //   </section>
-  // );
+            return (
+              <>asd</>
+              // <CarouselItem key={locker.id}>
+              //   <Card>
+              //     <CardTitle>
+              //       <Header locker={locker} store={store} />
+              //     </CardTitle>
+              //     <BoxContent locker={locker} reservas={reservas} />
+              //   </Card>
+              // </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
+    </section>
+  );
 }
 
 async function fetchData() {
