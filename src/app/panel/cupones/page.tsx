d495df1115e1b3a41e796de9c4362cc2ca15a7cc
Anselmo <anseldalbene@gmail.com>
2025-01-15 09:46:11 -0300

    test

--- src/app/panel/cupones/page.tsx
+++ src/app/panel/cupones/page.tsx
@@ -1,12 +1,13 @@
+"use client";
 import { Title } from "~/components/title";
 import { List, ListTile } from "~/components/list";
-import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
 import { Button } from "~/components/ui/button";
 import { PlusCircleIcon } from "lucide-react";
 
-export default async function Home() {
-  const cupones = await api.cupones.get.query();
+export default function Home() {
+  const { data: cupones } = api.cupones.get.useQuery();
   const router = useRouter();
   return (
     <section className="space-y-2">
@@ -18,7 +19,6 @@ export default async function Home() {
         </Button>{" "}
       </div>
       <List>
-        {}
         {cupones &&
           cupones.map((cupon) => {
             return (
--- src/components/app-sidenav.tsx
+++ src/components/app-sidenav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Sidenav, { SidenavItem, SidenavSeparator } from "./sidenav";
 import {
   AlignStartVerticalIcon,
