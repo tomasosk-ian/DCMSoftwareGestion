242808fb067a6283478a40df1a8aae3767fe97fb
Tomás Gutiérrez <tomasosk@iantech.com.ar>
2025-07-11 14:27:02 -0300

    fix/build router

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -6,7 +6,6 @@ import { PageClient } from "./page-client";
 import { getLocale } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -53,12 +52,7 @@ export default async function Page({
     where: eq(schema.stores.identifier, data.store_id),
   });
 
-  const router = useRouter();
   const allConfirmed = reserves.reduce((acc, r) => (typeof r.Confirmado === 'boolean' && r.Confirmado) && acc, true);
-  if (!allConfirmed) {
-    setTimeout(() => router.reload(), 5000);
-  }
-
   if (!store || reserves.length < 1 || !allConfirmed) {
     return <div></div>;
   }
