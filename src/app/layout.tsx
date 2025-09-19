c2476eb9fca6095280a391fbeddcdb0f36e0f612
Tomás Gutiérrez <tomasosk@iantech.com.ar>
2025-09-19 10:31:35 -0300

    update layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "DCM Solutions",
+  title: "Iguazú Lockers",
   description: "Sistema de reserva de lockers",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  // icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
