1664957b6690498fb578f46c7a72c32d1bebc2b4
Tomás Gutiérrez <tomasosk@iantech.com.ar>
2025-09-19 10:28:02 -0300

    update header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,77 +1,69 @@
 "use client"
-import ButtonCustomComponent from "~/components/buttonCustom";
-import { setLang } from "../actions";
 import { useTranslations } from "next-intl";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import { useSidebar } from "~/components/ui/sidebar";
 import { Languages } from "~/translations";
-import { SidebarTrigger, useSidebar } from "~/components/ui/sidebar";
+import { setLang } from "../actions";
 
 export default function Home({ lang }: { lang?: string }) {
   const t = useTranslations("HomePage");
   const { toggleSidebar } = useSidebar();
-
+  
   return (
     <div>
       <div
-        className="masthead inline-header center widgets large-mobile-menu-icon dt-parent-menu-clickable show-sub-menu-on-hover show-device-logo show-mobile-logo top-0"
+        className="flex flex-row inline-header center widgets full-height large-mobile-menu-icon dt-parent-menu-clickable show-sub-menu-on-hover show-device-logo show-mobile-logo top-0 bg-[#0F0C24]"
         role="banner"
       >
-        <header className="header-bar">
+        <header className="flex flex-row header-bar bg-[#0F0C24] w-full">
           <div className="branding">
             <div id="site-title" className="assistive-text">
-              Lockers Urbanos | Guarda equipajes en Bariloche
+              Iguazú Lockers
             </div>
             <div id="site-description" className="assistive-text">
               Una solución inteligente para el guardado de equipaje
             </div>
-            <a className="" href="https://lockersurbanos.com.ar/">
-              <img
-                className="preload-me"
-                src="./Lockers Urbanos _ Guarda equipajes en Bariloche – Una solución inteligente para el guardado de equipaje_files/logo-lockers.png"
-                width="207"
-                height="74"
-                sizes="207px"
-                alt="Lockers Urbanos | Guarda equipajes en Bariloche"
-              />
-              <img
-                className="mobile-logo preload-me"
-                src="./Lockers Urbanos _ Guarda equipajes en Bariloche – Una solución inteligente para el guardado de equipaje_files/logo_m.png"
-                width="183"
-                height="64"
-                sizes="183px"
-                alt="Lockers Urbanos | Guarda equipajes en Bariloche"
-              />
-            </a>
           </div>
-          <ul id="primary-menu" className="main-nav outside-item-remove-margin">
+          <ul id="primary-menu" className="flex flex-row main-nav outside-item-remove-margin">
+            <li>
+              <a className="" href="https://iguazulockers.com/">
+                <img
+                  src="./Lockers Urbanos _ Guarda equipajes en Bariloche – Una solución inteligente para el guardado de equipaje_files/logo-iguazu.png"
+                  width="207"
+                  height="74"
+                  sizes="207px"
+                  alt="Iguazú Lockers"
+                />
+              </a>
+            </li>
             <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-11 current_page_item menu-item-27 first depth-0">
-              <a href="https://lockersurbanos.com.ar/" data-level="1">
+              <a href="https://iguazulockers.com/" data-level="1">
                 <span className="menu-item-text">
-                  <span className="menu-text">{t("home")}</span>
+                  <span className="text-[#00E1A5]">{t("home")}</span>
                 </span>
               </a>
             </li>
             <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-30 depth-0">
-              <a href="https://lockersurbanos.com.ar/#!/lockers" data-level="1">
+              <a href="https://iguazulockers.com/#!/lockers" data-level="1">
                 <span className="menu-item-text">
-                  <span className="menu-text">{t("lockers")}</span>
+                  <span className="text-[#00E1A5]">{t("lockers")}</span>
                 </span>
               </a>
             </li>
             <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-29 depth-0">
               <a
-                href="https://lockersurbanos.com.ar/preguntas-frecuentes/"
+                href="https://iguazulockers.com/"
                 data-level="1"
               >
                 <span className="menu-item-text">
-                  <span className="menu-text">{t("faq")}</span>
+                  <span className="text-[#00E1A5]">{t("faq")}</span>
                 </span>
               </a>
             </li>
             <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-28 last depth-0">
-              <a href="https://lockersurbanos.com.ar/contacto/" data-level="1">
+              <a href="https://iguazulockers.com/" data-level="1">
                 <span className="menu-item-text">
-                  <span className="menu-text">{t("contact")}</span>
+                  <span className="text-[#00E1A5]">{t("contact")}</span>
                 </span>
               </a>
             </li>
@@ -80,7 +72,7 @@ export default function Home({ lang }: { lang?: string }) {
             <div className="soc-ico show-on-desktop in-top-bar-right in-menu-second-switch disabled-bg custom-border border-on hover-disabled-bg hover-custom-border hover-border-on first last">
               <a
                 title="Instagram page opens in new window"
-                href="https://lockersurbanos.com.ar/#"
+                href="https://iguazulockers.com/#"
                 target="_blank"
                 className="instagram"
               >
@@ -91,7 +83,7 @@ export default function Home({ lang }: { lang?: string }) {
               </a>
               <a
                 title="Facebook page opens in new window"
-                href="https://lockersurbanos.com.ar/#"
+                href="https://iguazulockers.com/#"
                 target="_blank"
                 className="facebook"
               >
@@ -117,30 +109,30 @@ export default function Home({ lang }: { lang?: string }) {
             </div>
           </div>
         </header>
-        <div className="mobile-header-bar">
+        <div className="mobile-header-bar bg-[#0F0C24]">
           <div className="mobile-navigation">
-            <button
+            <a
+              href="https://iguazulockers.com/#"
               className="dt-mobile-menu-icon"
               aria-label="Mobile menu icon"
-              onClick={() => toggleSidebar()}
             >
               <div className="lines-button">
                 <span className="menu-line"></span>
                 <span className="menu-line"></span>
                 <span className="menu-line"></span>
               </div>
-            </button>
+            </a>
           </div>
           <div className="mobile-mini-widgets"></div>
           <div className="mobile-branding">
-            <a className="" href="https://lockersurbanos.com.ar/">
+            <a className="" href="https://iguazulockers.com/">
               <img
                 className="preload-me"
                 src="./Lockers Urbanos _ Guarda equipajes en Bariloche – Una solución inteligente para el guardado de equipaje_files/logo-lockers.png"
                 width="207"
                 height="74"
                 sizes="207px"
-                alt="Lockers Urbanos | Guarda equipajes en Bariloche"
+                alt="Iguazú Lockers"
               />
               <img
                 className="mobile-logo preload-me"
@@ -148,7 +140,7 @@ export default function Home({ lang }: { lang?: string }) {
                 width="183"
                 height="64"
                 sizes="183px"
-                alt="Lockers Urbanos | Guarda equipajes en Bariloche"
+                alt="Iguazú Lockers"
               />
             </a>
           </div>
